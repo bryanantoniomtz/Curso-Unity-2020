934b2539b49301d9167ca13763b17c5e51232501
bryanantoniomtz <91036461+bryanantoniomtz@users.noreply.github.com>
2022-01-20 22:28:23 -0700

    Parte 2 subida proyectos

---
+++ 05_QuickClick/Assets/_Scripts/GameManager.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEditorInternal;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Random = UnityEngine.Random;
+
+public class GameManager : MonoBehaviour
+{
+    
+    private const string MAX_SCORE = "MAX_SCORE";
+
+    
+    public enum GameState
+    {
+         loading,
+         inGame,
+         gameOver
+    }
+
+    public GameState gameState;
+    
+    public List<GameObject> targetPrefabs;
+
+    private float spawnRate = 1.5f;
+    
+    public TextMeshProUGUI scoreText;
+    public Button restartButton;
+    
+    private int _score;
+    private int Score
+    {
+        set
+        {
+            _score = Mathf.Clamp(value, 0, 99999);
+        }
+        get
+        {
+            return _score;
+        }
+    }
+
+    public TextMeshProUGUI gameOverText;
+
+    public GameObject titleScreen;
+
+    private int numberOfLives = 4;
+    public List<GameObject> lives;
+    
+    private void Start()
+    {
+        ShowMaxScore();
+    }
+
+    /// <summary>
+    /// Método que inicia la partida cambiado el valor del estado del juego
+    /// </summary>
+    /// <param name="difficulty">Número entero que indica el grado de dificultad del juego</param>
+    public void StartGame(int difficulty)
+    {
+        gameState = GameState.inGame;
+        titleScreen.gameObject.SetActive(false);
+
+        //x op= y <-> x = x op y
+        spawnRate /= difficulty;
+        numberOfLives -= difficulty;
+
+        for (int i = 0; i < numberOfLives; i++)
+        {
+            lives[i].SetActive(true);
+        }
+        
+
+        StartCoroutine(SpawnTarget());
+
+        Score = 0;
+        UpdateScore(0);
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (gameState == GameState.inGame)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targetPrefabs.Count);
+            Instantiate(targetPrefabs[index]);
+        }
+    }
+
+
+
+    /// <summary>
+    /// Actualiza la puntuación y lo muestra por pantalla
+    /// </summary>
+    /// <param name="scoreToAdd">Número de puntos a añadir a la puntuación global</param>
+    public void UpdateScore(int scoreToAdd)
+    {
+        Score += scoreToAdd;
+        scoreText.text = "Score: \n" + Score;
+    }
+
+    
+    public void ShowMaxScore()
+    {
+        int maxScore = PlayerPrefs.GetInt(MAX_SCORE, 0);
+        scoreText.text = "Max Score: \n" + maxScore;
+    }
+
+    private void SetMaxScore()
+    {
+        int maxScore = PlayerPrefs.GetInt(MAX_SCORE, 0);
+        if (Score > maxScore)
+        {
+            PlayerPrefs.SetInt(MAX_SCORE, Score);
+            //TODO: Si hay nueva puntuación máxima, lanzar cohetes
+        } 
+        
+    }
+    public void GameOver()
+    {
+
+        numberOfLives--;
+
+        if (numberOfLives>=0)
+        {
+            Image heartImage = lives[numberOfLives].GetComponent<Image>();
+            var tempColor = heartImage.color;
+            tempColor.a = 0.3f;
+            heartImage.color = tempColor;
+        }
+        
+        
+        if (numberOfLives<=0)
+        {
+            SetMaxScore();
+        
+            gameState = GameState.gameOver;
+            gameOverText.gameObject.SetActive(true);
+            restartButton.gameObject.SetActive(true);
+        }
+        
+    }
+
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+    
+}
---
+++ 05_QuickClick/Assets/_Scripts/Target.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class Target : MonoBehaviour
+{
+
+    private Rigidbody _rigidbody;
+
+    private float minForce = 12,
+        maxForce = 16,
+        maxTorque = 10,
+        xRange = 4,
+        ySpawnPos = -6;
+
+    private GameManager gameManager;
+
+    [Range(-100,100)]
+    public int pointValue;
+
+    public ParticleSystem explosionParticle;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.AddForce(RandomForce(), ForceMode.Impulse);
+        _rigidbody.AddTorque(RandomTorque(), RandomTorque(),
+            RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
+
+        //gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
+        gameManager = FindObjectOfType<GameManager>();
+    }
+    
+    /// <summary>
+    /// Genera un vector aleatorio en 3D
+    /// </summary>
+    /// <returns>Fuerza aleatoria para arriba</returns>
+    private Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minForce, maxForce);
+    }
+    
+    /// <summary>
+    /// Genera un número aleatorio
+    /// </summary>
+    /// <returns>Valor aleatorio entre -maxTorque y maxTorque</returns>
+    private float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+    
+    /// <summary>
+    /// Genera una posición aleatoria
+    /// </summary>
+    /// <returns>Posición aleatoria en 3D, con la coordenada z = 0</returns>
+    private Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange,xRange), ySpawnPos);
+    }
+
+    private void OnMouseOver()
+    {
+        if (gameManager.gameState == GameManager.GameState.inGame)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("KillZone"))
+        {
+            Destroy(gameObject);
+            if (gameObject.CompareTag("Good"))
+            {
+                gameManager.GameOver();
+            }
+        }
+    }
+}

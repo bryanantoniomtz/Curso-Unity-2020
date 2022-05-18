68fdab856377f867aae12c9b1976613609522584
bryanantoniomtz <91036461+bryanantoniomtz@users.noreply.github.com>
2022-05-18 11:00:22 -0700

    18052022 4

--- John Lemon/Assets/Scripts/PlayerControler.cs
+++ John Lemon/Assets/Scripts/PlayerControler.cs
@@ -1,3 +1,9 @@
+#if UNITY_IOS || UNITY_ANDROID
+    #define USING_MOBILE
+
+#endif
+
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -23,8 +29,21 @@ public class PlayerControler : MonoBehaviour
     
     void FixedUpdate()
     {
-        float horizontal=Input.GetAxis("Horizontal");
-        float vertical=Input.GetAxis("Vertical");
+    
+    #if USING_MOBILE
+      float horizontal=Input.GetAxis("Mouse X");
+      float vertical=Input.GetAxis("Mouse Y");
+      if(Input.touchCount>0){
+         horizontal=Input.touches[0].deltaPosition.x;
+         vertical=Input.touches[0].deltaPosition.y;
+      }
+      
+    #else
+       float horizontal=Input.GetAxis("Horizontal");
+       float vertical=Input.GetAxis("Vertical");
+    #endif
+    
+       
         movement.Set(horizontal,0,vertical);
         movement.Normalize();
         

d090cf571c8fb1f442692df410d55d17a807bc54
bryanantoniomtz <91036461+bryanantoniomtz@users.noreply.github.com>
2022-05-18 09:55:46 -0700

    18052022 1

--- 02_Wildlife/Assets/Scripts/MoveForward.cs
+++ 02_Wildlife/Assets/Scripts/MoveForward.cs
@@ -4,10 +4,35 @@ using UnityEngine;
 
 public class MoveForward : MonoBehaviour
 {
-    public float speed = 40f;
-
+    public float speed = 10f;
+    
+    /*  private void OnTriggerEnter(Collider other){
+                Debug.Log("GAME OVER!!!!");
+          
+                if(other.gameObject.CompareTag("Atack")){
+                Debug.Log("GAME OVER2!!!!");
+                  Destroy(this.gameObject);
+                }
+     }*/
+    
     void Update()
     {
-        transform.Translate(Vector3.forward*speed*Time.deltaTime);
+      if(this.gameObject.CompareTag("Enemy")){
+          transform.Translate(Vector3.forward*speed*Time.deltaTime);
+      }else{
+          transform.Translate(Vector3.backward*speed*Time.deltaTime);
+      }      
     }
+    
+   /*   private void OnTriggerEnter(Collider other)
+              {
+                 Debug.Log(other.name + " entered the trigger " + name);
+                     if(other.gameObject.CompareTag("Enemy")){
+                            Destroy(other.gameObject);
+                     }
+              }
+      */
+  
+                      
+    
 }
